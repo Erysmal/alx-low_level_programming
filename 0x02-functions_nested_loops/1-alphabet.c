fa92a6c6a2e227f346058d167c2fdc8ac4513c4c
Erysmal <benedictemmanuel7110@gmail.com>
2023-07-19 11:03:19 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * print_alphabet - print small alphabet
+  *
+  */
+void print_alphabet(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
