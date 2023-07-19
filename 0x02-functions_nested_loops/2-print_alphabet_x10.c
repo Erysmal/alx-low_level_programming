97abcb93a058ec24fb32a4925df7ce48807cf41c
Erysmal <benedictemmanuel7110@gmail.com>
2023-07-19 12:02:53 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 	int x10;
 	char letters;
 
-	for (x10 = 0; x10 <= 10; x10++)
+	for (x10 = 0; x10 < 10; x10++)
 	{
 		for (letters = 'a'; letters <= 'z'; letters++)
 		{
