a12121232c9cd4d709ba0d34e14def3d6044539b
Erysmal <benedictemmanuel7110@gmail.com>
2023-07-20 07:21:16 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - Function that compites absolutes value
+ *
+ * @n: Character to be checked
+ *
+ * Return: Onky positive number
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+
+}
