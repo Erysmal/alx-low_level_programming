01b3bb6d2c2ecaa7fca8da42d23b8a848201687a
Erysmal <benedictemmanuel7110@gmail.com>
2023-07-20 07:13:04 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - Check for alphabets
+ *
+ * @c: The Character to be checked
+ *
+ *  Return: 1 if either lower or uppercase alphabet
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
