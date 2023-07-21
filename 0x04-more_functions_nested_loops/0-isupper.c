357e08b82e01fc50e2a5e0aea31751ce78ce25a0
Erysmal <benedictemmanuel7110@gmail.com>
2023-07-21 14:32:42 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper - Checkes for uppercase character
+ * @c:character to be checked
+ * Return: 1 if c is uppercase otherwise return 0
+ */
+int _isupper(int c)
+{
+	if (c >= "A" && c <= "z")
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
