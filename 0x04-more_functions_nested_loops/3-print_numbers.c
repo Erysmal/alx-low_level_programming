67c025b163d072e41cf377a87bf0c08682e4bd2b
Erysmal <benedictemmanuel7110@gmail.com>
2023-07-21 15:40:59 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - from 0 to 9
+ *
+ * Return: Always 0 (success)
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(\n);
+}
