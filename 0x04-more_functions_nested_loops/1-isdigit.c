7bdb8a075e474909bf62a828f8a1fd70ad7dba0c
Erysmal <benedictemmanuel7110@gmail.com>
2023-07-21 15:17:04 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,13 +2,13 @@
 /**
  * _isdigit - Checks for a digit
  *
- @c: parameter to be checked
+ *@c: parameter to be checked
  *
  *Return: (1) if c is a digit else 0
  */
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}
