3e603561ca47c823c93a4dca27b2a3b25f39949e
Erysmal <benedictemmanuel7110@gmail.com>
2023-07-24 15:11:04 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * _puts - Function that prints a string with new line
+ * @str: String to be printed
+ * Return:
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
