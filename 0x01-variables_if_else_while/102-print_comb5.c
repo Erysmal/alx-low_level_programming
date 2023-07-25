6687598a0be4305ac144be72a4ddf3904fb03e36
Erysmal <benedictemmanuel7110@gmail.com>
2023-07-25 12:02:54 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,7 +16,7 @@ int main(void)
 		{
 			for (digit3 = digit1; digit3 <= 9; digit3++)
 			{
-				for (digit4 = (digit3 == digit1) ? (digit2 +1) : 0; digit4 <= 9; digit4++)
+				for (digit4 = (digit3 == digit1) ? (digit2 + 1) : 0; digit4 <= 9; digit4++)
 				{
 					putchar(digit1 + '0');
 					putchar(digit2 + '0');
