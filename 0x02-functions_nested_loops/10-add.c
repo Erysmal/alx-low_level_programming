b68ff1aae3e0e9f361481cbe3a6e764d25fa3a85
Erysmal <benedictemmanuel7110@gmail.com>
2023-07-25 21:59:58 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Function that adds to integer
+ *
+ * @n: First integer
+ * @n2: Second integer
+ *
+ * Return: The result of addition of two integer
+ */
+int add(int n, int n2)
+{
+	int result = n + n2;
+
+	return (result);
+}
