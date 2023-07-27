995ddb680d2b45b3d5e12604757335955b7297c9
Erysmal <benedictemmanuel7110@gmail.com>
2023-07-27 06:09:26 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,21 +5,4 @@
  * @n: Integer
  * Return: nothing
  */
-void print_number(int n)
-{
-	unsigned int n1;
-
-	n1 = n;
-
-	if (n < 0)
-	{
-		_putchar('_');
-		n1 = -n;
-	}
-
-	if (n1 / 10 != 0)
-	{
-		print_number(n1 / 10);
-	}
-	_putchar((n1 % 10) + '0');
-}
+void p
