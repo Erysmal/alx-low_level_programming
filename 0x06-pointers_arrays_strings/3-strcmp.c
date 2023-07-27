de480034506d95c311c9367f6fe20964e5b84bca
Erysmal <benedictemmanuel7110@gmail.com>
2023-07-26 23:55:32 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - Function that compares 2 string
+ * @s1: String to be compared
+ * @s2: String to be compared
+ * Return: (*s1 -s2)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' || *s2 != '\0')
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+
+		s1++;
+		s2++;
+	}
+
+	return (0);
+
+}
