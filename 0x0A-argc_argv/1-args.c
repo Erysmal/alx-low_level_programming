33741ca86a3fc2e32c964076171fba4f666b80c1
Erysmal <benedictemmanuel7110@gmail.com>
2023-08-06 19:33:08 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,10 +5,8 @@
  *@argv: Actual arguments
  *Return: Always 0 (success)
  */
-int main(int argc, char **argv)
+int main(int argc, char **argv __attribute__((unused)))
 {
-	(void)argv;
-
 	printf("%d \n", argc - 1);
 
 	return (0);
