964e92ad2cfa9d45703d3d16ea057d7c789fbf78
Erysmal <benedictemmanuel7110@gmail.com>
2023-08-06 19:02:17 -0700

    mynewnameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	while(argc--)
+	{
+		printf("%s\n",argv[0]);
+	}
+	return (0);
+}
