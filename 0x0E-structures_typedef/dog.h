4a0e4ccd984eb375596fe52e4e3ab01bf78da075
Erysmal <benedictemmanuel7110@gmail.com>
2023-08-14 19:50:23 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+
+/**
+ * struct dog - Represents a dog with its name, age, and owner.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+
 struct dog
 {
 	char *name;
