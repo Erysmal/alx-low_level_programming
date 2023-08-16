46c3c5d9cfcd7068698d3536b4c8c7645849b9d0
Erysmal <benedictemmanuel7110@gmail.com>
2023-08-16 13:58:30 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - function pointer that prints name
+ * @name: name to be printed
+ * @f: pointer function
+ * Return: (void)
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
