947699f5bb477a844aa1595d404b70c14805e5f4
Erysmal <benedictemmanuel7110@gmail.com>
2023-08-17 00:08:05 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - function that search for integer
+ * @array: array
+ * @size: size of array
+ * @cmp: pointer function
+ * Return: index of the fisrt element if cmp did not return
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	if (array && size)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+		}	}
+	}
+
+	return (-1);
+}
