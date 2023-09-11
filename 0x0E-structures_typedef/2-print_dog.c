54a873d9db5d8af32dee231e5ddbcd7413657d93
Erysmal <benedictemmanuel7110@gmail.com>
2023-09-11 08:30:33 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,34 +6,34 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
+	if (d == NULL)
 	{
-		if (d->name != NULL)
-		{
-			printf("Name: %s\n", d->name);
-		}
-		else
-		{
-			printf("Name: (nil)\n");
-		}
+		return;
+	}
+	if (d->name != NULL)
+	{
+		printf("Name: %s\n", d->name);
+	}
+	else
+	{
+		printf("Name: (nil)\n");
+	}
 
-		if (d->age)
-		{
-			printf("Age: %f\n", d->age);
-		}
-		else
-		{
-			printf("Age: (nil)\n");
-		}
+	if (d->age)
+	{
+		printf("Age: %f\n", d->age);
+	}
+	else
+	{
+		printf("Age: (nil)\n");
+	}
 
-		if (d->owner != NULL)
-		{
-			printf("Owner: %s\n", d->owner);
-		}
-		else
-		{
-			printf("Name: (nil)\n");
-		}
+	if (d->owner != NULL)
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+	else
+	{
+		printf("Owner: (nil)\n");
 	}
 }
-
